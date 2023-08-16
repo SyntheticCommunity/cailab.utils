c7c1fbf4b995b665872e1fb75ead4e62f9b536bf
Chun-Hui Gao <gaospecial@gmail.com>
2023-08-16 14:25:22 +0800

    add statistical utils

---
+++ R/statistical_plot.R
@@ -0,0 +1,78 @@
+# library(agricolae)
+# data(sweetpotato)
+# model<-aov(yield~virus,data=sweetpotato)
+# out <- duncan.test(model,"virus",
+#                    main="Yield of sweetpotato. Dealt with different virus")
+# plot(out,variation="IQR")
+
+#' Plot a boxplot and add group to it
+#'
+#' See also: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6193594/
+#'
+#' @param data data
+#' @param method aov
+#' @param group group variable, should be a column in data
+#' @param value values, should be a column in data
+#' @param with_jitter add jitter points
+#' @param label_repel use ggrepel to plot label
+#'
+#' @return a ggplot object
+#' @export
+#'
+#' @importFrom ggplot2 ggplot aes geom_boxplot geom_jitter geom_text
+#' @importFrom ggrepel geom_text_repel
+#' @importFrom dplyr mutate left_join select
+#' @importFrom tibble rownames_to_column
+#' @importFrom rlang .data
+#'
+#' @examples
+#'  data("PlantGrowth")
+#'  gg_boxplot_with_group(PlantGrowth, group = "group", value = "weight")
+gg_boxplot_with_group = function(data,
+                                 group,
+                                 value,
+                                 method = "aov",
+                                 with_jitter = TRUE,
+                                 label_repel = TRUE){
+  # run statistical analysis
+  model = do.call(what = method,
+                  list(formula = stats::formula(paste0(value, " ~ ", group)),
+                       data = data))
+  # get groups
+  out = agricolae::duncan.test(model,
+                               trt = group)
+
+  # prepare group data
+  groups = out$groups %>%
+    rownames_to_column(var = {{ group }})
+  label_pos = out$means %>%
+    rownames_to_column(var = {{ group }}) %>%
+    mutate(group_pos = .data$Max + .data$se) %>%  # use Max + se for place group label
+    left_join(groups, by = {{ group }}) %>%
+    select(c(group, "groups", "group_pos"))
+
+  # plot
+  p = ggplot(data = data,
+             aes(x = .data[[group]],  # I don't know why {{}} isn't work
+                 y = .data[[value]])) +
+    geom_boxplot(outlier.shape = NA)
+
+  if (with_jitter){
+    p = p + geom_jitter(alpha = 0.5)
+  }
+
+  if (label_repel){
+    label_geom = ggrepel::geom_text_repel
+  } else {
+    label_geom = geom_text
+  }
+
+  p = p + label_geom(
+    mapping = aes(x = .data[[group]],
+                  y = .data$group_pos,
+                  label = groups),
+    data = label_pos)
+
+  return(p)
+}
+
