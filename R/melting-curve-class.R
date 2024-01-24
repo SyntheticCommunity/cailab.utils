29f82c831e5536b015fac65f8034393842854db0
Chun-Hui Gao <gaospecial@gmail.com>
2024-01-24 20:23:27 +0800

    more methods for MC

--- R/melting-curve-class.R
+++ R/melting-curve-class.R
@@ -153,8 +153,8 @@ mc2tbl = function(mc){
   if (!is.na(date)) tbl = dplyr::mutate(tbl, date = date)
   if (!is.na(primer)) tbl = dplyr::mutate(tbl, primer = primer)
   if (!is.na(plate)) tbl = dplyr::left_join(tbl, plate, by = "well_position")
-    dplyr::left_join(, by = "well_position") |>
-    tidyr::unite(well_position, tidyr::any_of(c("date","primer","plate","well_position")))
+  tbl = tbl |>
+    tidyr::unite(col = "well_position", tidyr::any_of(c("date","primer","plate","well_position")))
   return(tbl)
 }
 
@@ -171,4 +171,127 @@ plot_mc = function(mc, y = "derivative", show_tm = FALSE){
   df = getData(mc)
   if (!is.na(plate)) df = dplyr::left_join(df, plate, by = "well_position")
   plot_quantstudio_melting_curve(df, y = y, show_tm = show_tm)
+}
+
+
+
+#' Tibble to wider matrix-like format
+#'
+#' @param mc object
+#' @param column_names_from column names
+#' @param column_values_from column values
+#'
+#' @return a wider tibble
+#' @export
+mc_tbl2wider = function(mc,
+                        column_names_from = "temperature",
+                        column_values_from = "derivative") {
+  tbl = getData(mc) |>
+    tidyr::pivot_wider(names_from = column_names_from, names_prefix = "T", values_from = column_values_from)
+  plate = getPlate(mc)
+  if (is.data.frame(plate)) tbl = tbl |> dplyr::left_join(plate, by = "well_position")
+  return(tbl)
+}
+
+
+#' PCA analysis of MeltingCurve object
+#'
+#' @param object MeltingCurve object
+#' @inheritParams vegan::rda()
+#'
+#' @return
+#' @export
+#'
+#' @examples
+mc_pca = function(object, scale){
+  if (!inherits(object, "MeltingCurve")) stop("Object is not a MeltingCurve class object.")
+  tbl = getData(object) |> mc_tbl2wider()
+  mat = as.matrix(tbl |> dplyr::select(dplyr::starts_with("T")))
+  rownames(mat) = tbl$well_position
+  pca = vegan::rda(mat, scale = scale)
+  return(pca)
+}
+
+#' @title plot PCA result with extra data (Plate/Sample table)
+#'
+#' @param object MC
+#' @param scale whether scale data in PCA
+#' @param extra extra data table (join by 'well_position')
+#' @param color color mapping
+#' @param shape shape mapping
+#' @param show_temperature show temperature arrow
+#' @param temp_n limit for temperature arrows
+#'
+#' @export
+#' @return ggplot
+#' @import ggplot2
+mc_pca_plot = function(object, scale = FALSE,
+                      extra = NULL,
+                      color = NULL,
+                      shape = NULL,
+                      show_temperature = FALSE,
+                      temp_n = 3) {
+  pca = mc_pca(object, scale = scale)
+  sites = .pca_site_score(pca, site_table = extra)
+  species = .pca_species_score(pca, temp_n = temp_n)
+
+  p = ggplot()
+  if (is.null(sample_table)) {
+    p = p + geom_point(data = sites, mapping = aes(x = .data$PC1, y = .data$PC2))
+  } else {
+    p = p + geom_point(data = sites, mapping = aes(x = .data$PC1, y = .data$PC2, shape = .data[[shape]], color = .data[[color]]))
+  }
+
+  if (show_temperature) {
+    p = p + geom_segment(data = species,
+                         mapping = aes(x = 0, y = 0, xend = .data$PC1, yend = .data$PC2),
+                         arrow = arrow(length = unit(0.1,"cm")), show.legend = F) +
+      ggrepel::geom_text_repel(mapping = aes(x = .data$PC1, y = .data$PC2, label = .data$temperature),
+                               max.overlaps = 30,
+                               inherit.aes = FALSE,
+                               data = species, show.legend = F)
+  }
+
+  return(p)
+}
+
+.pca_site_score = function(pca, site_table = NULL) {
+  pca.df = vegan::scores(pca, choices = 1:2, display = "all", tidy = TRUE)
+  sites = pca.df |> dplyr::filter(.data$score == "sites") |>
+    dplyr::select(dplyr::starts_with("PC")) |>
+    tibble::rownames_to_column("well_position") |>
+    dplyr::as_tibble()
+  if (!is.null(site_table)) sites = sites |> dplyr::left_join(site_table, by = "well_position")
+  return(sites)
+}
+
+.pca_species_score = function(pca, temp_n) {
+  pca.df = vegan::scores(pca, choices = 1:2, display = "all", tidy = TRUE)
+  species = pca.df |> dplyr::filter(.data$score == "species") |>
+    dplyr::select(dplyr::starts_with("PC")) |>
+    tibble::rownames_to_column("temperature") |>
+    dplyr::as_tibble() |>
+    dplyr::rowwise() |>
+    dplyr::mutate(distance = .data$PC1 ^ 2 + .data$PC2 ^ 2,
+                  quadrant = get_quadrant(.data$PC1, .data$PC2)) |>
+    dplyr::ungroup()
+  if (is.numeric(temp_n)) species = species |> dplyr::slice_max(.data$distance, n = temp_n, by = "quadrant")
+  return(species)
+}
+
+
+# get quadrant of a 2D vector
+get_quadrant <- function(x, y) {
+  if (x > 0 && y > 0) {
+    quadrant <- 1
+  } else if (x < 0 && y > 0) {
+    quadrant <- 2
+  } else if (x < 0 && y < 0) {
+    quadrant <- 3
+  } else if (x > 0 && y < 0) {
+    quadrant <- 4
+  } else {
+    quadrant <- 0  # 如果在坐标轴上，也可以考虑返回0或NA，根据需要调整
+  }
+  return(quadrant)
 }
